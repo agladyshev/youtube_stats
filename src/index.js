b0aceb934bd7e9076c8dadd7703282385bcaf5ac
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2018-05-31 15:43:55 +0300

    feat: Update db with youtube profile info

--- src/index.js
+++ src/index.js
@@ -6,13 +6,6 @@ const moment = require('moment');
 const request = require('request');
 require('dotenv').config();
 
-// const client = new Youtube({
-//   consumer_key: process.env.YOUTUBE_CONSUMER_KEY,
-//   consumer_secret: process.env.YOUTUBE_CONSUMER_SECRET,
-//   access_token_key: process.env.YOUTUBE_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.YOUTUBE_ACCESS_TOKEN_SECRET,
-// });
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}/test?retryWrites=true`;
 
 const getYoutubeAccounts = async () => {
@@ -39,10 +32,10 @@ const getYoutubeProfile = async () => {
   const accounts = await getYoutubeAccounts();
   accounts.forEach((account) => {
     try {
-      const uri = account.twitter_id ?
+      const uri = account.youtube_id ?
         `https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=${account.youtube_id}&key=${process.env.API_KEY}` :
         `https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&forUsername=${account.youtube_name}&key=${process.env.API_KEY}`
-    
+
       request(uri, function (error, response, body) {
 
         if ((body === undefined) || (body === null)) {
@@ -94,9 +87,8 @@ const getYoutubeProfile = async () => {
           youtube_thumbnail_high,
           youtube_status,
         });
-
         updateProfile(account);
-       
+      
       });
     } catch (e) {
       console.error(e);
@@ -105,94 +97,36 @@ const getYoutubeProfile = async () => {
 };
 
 const updateProfile = async (account) => {
-  console.log(account);
   // Update youtube stats in database
-  // try {
-  //   const client = await MongoClient.connect(uri, { useNewUrlParser: true });
-  //   const db = client.db(process.env.DB_NAME);
-  //   const col = db.collection('influencers');
-  //   col.updateOne(
-  //     { _id: account._id }
-  //     , {
-  //       $set: {
-  //         youtube_id: account.youtube_id,
-  //         youtube_name: account.youtube_name,
-  //         youtube_followers: account.youtube_followers,
-  //         tweets: account.tweets,
-  //         youtube_pic: account.youtube_pic,
-  //         youtube_status: account.youtube_status,
-  //         youtube_updated: Date.now(),
-  //       }
-  //     }, function(err, result) {
-  //       assert.equal(err, null);
-  //       assert.equal(1, result.result.n);
-  //   });
-  //   client.close();
-  // } catch (e) {
-  //   console.error(e);
-  // }
+  try {
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const db = client.db(process.env.DB_NAME);
+    const col = db.collection('influencers');
+    col.updateOne(
+      { _id: account._id }
+      , {
+        $set: {
+          youtube_id: account.youtube_id,
+          youtube_name: account.youtube_name,
+          youtube_description: account.youtube_description,
+          youtube_views: account.youtube_views,
+          youtube_subscribers: account.youtube_subscribers,
+          youtube_videos: account.youtube_videos,
+          youtube_thumbnail: account.youtube_thumbnail,
+          youtube_thumbnail_med: account.youtube_thumbnail_med,
+          youtube_thumbnail_high: account.youtube_thumbnail_high,
+          youtube_status: account.youtube_status,
+          youtube_updated: Date.now(),
+        },
+      }, function(err, result) {
+        assert.equal(err, null);
+        assert.equal(1, result.result.n);
+    });
+    client.close();
+  } catch (e) {
+    console.error(e);
+  }
 };
-
-// const getTweetStats = async () => {
-//   // Call youtube for user tweets
-//   const accounts = await getYoutubeAccounts();
-//   accounts.forEach((account) => {
-//     try {
-//       client.get(
-//         `https://api.youtube.com/1.1/statuses/user_timeline.json?user_id=${account.youtube_id}&trim_user=true&exclude_replies=true&include_rts=false`,
-//         function (error, tweets, response) {
-
-//         const youtube_stats = tweets.reduce(function(accumulator, tweet, index) {
-//           // Filter tweets with a selected period of time
-//           if (moment(tweet.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en')
-//             .isAfter(moment().subtract(process.env.STATS_SINCE_DAYS, 'days'))) {
-//             accumulator.retweets_recent = accumulator.retweets_recent + tweet.retweet_count;
-//             accumulator.favorites_recent = accumulator.favorites_recent + tweet.favorite_count;
-//             accumulator.tweets_recent += 1;
-//           }
-//           return accumulator;
-//         }, {retweets_recent: 0, favorites_recent: 0, tweets_recent: 0});
-//         if (error) {
-//           const youtube_status = error[0].message;
-//           account = Object.assign(account, {youtube_status});
-//         } else {
-//           const youtube_status = 'OK';
-//           account = Object.assign(account, youtube_stats, {youtube_cycle: process.env.STATS_SINCE_DAYS});
-//         }
-//         updateTweetStats(account);
-//       });
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   });
-// };
-
-// const updateTweetStats = async (account) => {
-//   // Update tweets stats in the database
-//   try {
-//     const client = await MongoClient.connect(uri, { useNewUrlParser: true });
-//     const db = client.db(process.env.DB_NAME);
-//     const col = db.collection('influencers');
-//     col.updateOne(
-//       { _id: account._id }
-//       , {
-//         $set: {
-//           youtube_retweets_recent: account.retweets_recent,
-//           youtube_favorites_recent: account.favorites_recent,
-//           tweets_recent: account.tweets_recent,
-//           youtube_cycle: account.youtube_cycle,
-//           youtube_status: account.youtube_status,
-//           tweets_updated: Date.now(),
-//         }
-//       }, function(err, result) {
-//         assert.equal(err, null);
-//         assert.equal(1, result.result.n);
-//     });
-//     client.close();
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
 
 const app = express();
 app.use(bodyParser.json());
@@ -205,12 +139,6 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   getYoutubeProfile();
-
-  // getYoutubeProfile();
-  // getTweetStats();
 });
-
-// app.get('/tweets', (req, res) => getTweetStats());
-// app.get('/profiles', (req, res) => getYoutubeProfile());
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
